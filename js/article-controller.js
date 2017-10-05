306a402f3c137dbdc8ea958f1b83509d46a4eda0
James Ransome <jamesransome@msn.com>
2017-10-05 16:19:29 +0100

    restructure article storage, pass test for show method

--- js/article-controller.js
+++ js/article-controller.js
@@ -1,19 +1,27 @@
 (function(exports){
   function ArticleController(articleModel) {
     this._articleModel = articleModel;
-    this._storedArticles = [];
+    this._storedArticles = {};
+    this._IdCounter = 0;
   }
 
   ArticleController.prototype = {
+    createNewArticleID: function(){
+      return this._IdCounter++;
+    },
+
     index: function(){
       return this._storedArticles;
     },
 
     new: function(articleData){
       var newArticle = new this._articleModel(articleData);
-      this._storedArticles.push(newArticle);
+      this._storedArticles[this.createNewArticleID()] = newArticle;
     },
 
+    show: function(id){
+      return this._storedArticles[id];
+    }
   };
 
   exports.ArticleController = ArticleController;
--- spec/unit/article-controller-spec.js
+++ spec/unit/article-controller-spec.js
@@ -3,7 +3,10 @@ function ArticleMock(data){
 }
 
 var mockData = "article data";
-var mockStoredArticles = [new ArticleMock(mockData), new ArticleMock(mockData)];
+var mockArticle1 = new ArticleMock(mockData);
+var mockArticle2 = new ArticleMock(mockData);
+var mockStoredArticles = {0: mockArticle1, 1: mockArticle2};
+var articleIdQuery = 1;
 
 describe("Article manager", function(){
   it("#new: creates a new article with given data", function(){
@@ -18,4 +21,9 @@ describe("Article manager", function(){
     return expect(articleController.index()).toEqual(mockStoredArticles);
   });
 
+  it("#show: returns the article with the given id", function(){
+    var articleController = new ArticleController(ArticleMock);
+    articleController._storedArticles = mockStoredArticles;
+    return expect(articleController.show(articleIdQuery)).toEqual(mockArticle2);
+  });
 });
