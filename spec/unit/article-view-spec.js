0d19d67483b15efba9c7fcf0c9154d5817c72aa0
James Ransome <jamesransome@msn.com>
2017-10-08 13:29:18 +0100

    add back button to article view

--- spec/unit/article-view-spec.js
+++ spec/unit/article-view-spec.js
@@ -4,7 +4,9 @@ var articleMockContent = 'Article content... blah blah';
 articleMock.stubFunctionAndReturn('title', articleMockTitle);
 articleMock.stubFunctionAndReturn('content', articleMockContent);
 
-var expectedHtml = "<h1>" + articleMockTitle + "</h1><div class='article-content'><p>" + articleMockContent + "</p></div>";
+var backButton = "<button onclick='articleController.index()'>Back to headlines</button>";
+var articleHtml = "<h1>" + articleMockTitle + "</h1><div class='article-content'><p>" + articleMockContent + "</p></div>";
+var expectedHtml = backButton + articleHtml;
 
 describe("Article view", function(){
   it("#toHtml: renders the HTML for a single article", function(){
