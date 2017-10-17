52d2dfaf327a8107aa9abcc7e498f1e2f7bfdaa5
James Ransome <jamesransome@msn.com>
2017-10-17 15:59:14 +0100

    add tests for api requester

--- js/api-requester.js
+++ js/api-requester.js
@@ -1,7 +1,8 @@
  (function(exports){
-  function ApiRequester(headlinesRequestUrl, summaryRequestUrl){
+  function ApiRequester(headlinesRequestUrl, summaryRequestUrl, xmlHttpRequestObj){
     this._headlinesRequestUrl = headlinesRequestUrl;
     this._summaryRequestUrl = summaryRequestUrl;
+    this._xmlHttpRequestObj = xmlHttpRequestObj || new XMLHttpRequest();
   }
 
   ApiRequester.prototype = {
@@ -18,16 +19,14 @@
     },
 
     makeRequest: function(url, callback){
-      var xhttp = new XMLHttpRequest();
-
-      xhttp.onreadystatechange = function() {
+      this._xmlHttpRequestObj.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
           callback(JSON.parse(this.responseText));
         }
       };
 
-      xhttp.open("GET", url, true);
-      xhttp.send();
+      this._xmlHttpRequestObj.open("GET", url, true);
+      this._xmlHttpRequestObj.send();
     }
   };
 
